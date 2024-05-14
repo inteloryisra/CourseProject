7d404b2330129eaead24deb5846a86449a88b99d
EjupiAnela <anela.ejupi@intelory.com>
2024-05-14 15:58:02 +0200

    fixe errors

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    public function register(Request $request): JsonResponse
-    {
 
+    public UserService $userService;
+
+    public function __construct(UserService $userService){
+        $this->userService = $userService;
+    }
+
+
+    public function register(Request $request)
+    {
             $validatedData = $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|email',
                 'password' => 'required|string|max:255',
             ]);
 
-           dd($validatedData);
-
+           return $this->userService->registerUser($validatedData);
     }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -6,27 +6,8 @@ use App\Models\User;
 
 class UserService
 {
-    public function registerUser($userData)
+    public function registerUser($data)
     {
-
-        $user = User::create([
-            'name' => $userData['name'],
-            'email' => $userData['email'],
-            'password' => bcrypt($userData['password']),
-        ]);
-
-
-        if ($user) {
-            return [
-                'success' => true,
-                'user' => $user,
-                'message' => 'User registered successfully!',
-            ];
-        } else {
-            return [
-                'success' => false,
-                'message' => 'Failed to register user.',
-            ];
-        }
+        return User::query()->create($data);
     }
 }
