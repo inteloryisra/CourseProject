353ac00a19903d1ca5c9a4bc85b4ac619acc93ce
EjupiAnela <anela.ejupi@intelory.com>
2024-05-22 10:10:43 +0200

    creted plans

---
+++ app/Models/Plan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'price',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_plans');
+    }
+}
---
+++ database/seeders/PlansTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlansTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $plans = [
+            ['name' => 'Basic', 'price' => 29.99],
+            ['name' => 'Premium', 'price' => 49.99],
+            ['name' => 'Enterprise', 'price' => 99.99],
+        ];
+
+        // Insert sample data into the database
+        foreach ($plans as $plan) {
+            DB::table('plans')->insert($plan);
+        }
+    }
+}
